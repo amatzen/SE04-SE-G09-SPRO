97c788a145b4698ea166721af1a694d1a7321639
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-03-20 01:50:18 +0100

    Added IEngine

---
+++ gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/services/managers/IEngine.java
@@ -0,0 +1,32 @@
+package dk.sdu.mmmi.swe.gtg.common.services.managers;
+
+import dk.sdu.mmmi.swe.gtg.common.data.Entity;
+import dk.sdu.mmmi.swe.gtg.common.data.GameData;
+import dk.sdu.mmmi.swe.gtg.common.family.Family;
+import dk.sdu.mmmi.swe.gtg.common.services.entity.IEntityProcessingService;
+import dk.sdu.mmmi.swe.gtg.common.services.entity.IPostEntityProcessingService;
+
+import java.util.List;
+
+public interface IEngine {
+
+    void update(GameData gameData);
+
+    String addEntity(Entity entity);
+
+    void removeEntity(String entityID);
+
+    void removeEntity(Entity entity);
+
+    Entity getEntity(String ID);
+
+    List<Entity> getEntitiesFor(Family family);
+
+    void addEntityProcessingService(IEntityProcessingService service);
+
+    void removeEntityProcessingService(IEntityProcessingService service);
+
+    void addPostEntityProcessingService(IPostEntityProcessingService service);
+
+    void removePostEntityProcessingService(IPostEntityProcessingService service);
+}
