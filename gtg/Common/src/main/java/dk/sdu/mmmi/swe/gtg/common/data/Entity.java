88b76b9da01c33fffebe2c888b37d23f566a6097
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-03-20 01:46:40 +0100

    Updated entity

--- gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/Entity.java
+++ gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/Entity.java
@@ -1,62 +1,85 @@
 package dk.sdu.mmmi.swe.gtg.common.data;
 
 import dk.sdu.mmmi.swe.gtg.common.data.entityparts.EntityPart;
+import dk.sdu.mmmi.swe.gtg.common.family.Family;
+import dk.sdu.mmmi.swe.gtg.common.signals.Signal;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Entity implements Serializable {
-    private final UUID ID = UUID.randomUUID();
+    private final UUID ID;
 
-    private float[] shapeX = new float[4];
-    private float[] shapeY = new float[4];
-    private float radius;
+    private Set<Family> families;
+
     private Map<Class, EntityPart> parts;
-    
+
+    public final Signal<Entity> onPartAdded;
+    public final Signal<Entity> onPartRemoved;
+
     public Entity() {
+        ID = UUID.randomUUID();
         parts = new ConcurrentHashMap<>();
+        families = new HashSet<>();
+
+        onPartAdded = new Signal<>();
+        onPartRemoved = new Signal<>();
     }
     
-    public void add(EntityPart part) {
+    public void addPart(EntityPart part) {
         parts.put(part.getClass(), part);
+        onPartAdded.fire(this);
     }
 
-    public void remove(Class partClass) {
+    public void removePart(Class partClass) {
         parts.remove(partClass);
+        onPartRemoved.fire(this);
     }
     
     public <E extends EntityPart> E getPart(Class<E> partClass) {
         return (E) parts.get(partClass);
     }
-    
-    public void setRadius(float r){
-        this.radius = r;
-    }
-    
-    public float getRadius(){
-        return radius;
+
+    public boolean hasPart(Class partClass) {
+        return parts.containsKey(partClass);
     }
 
     public String getID() {
         return ID.toString();
     }
 
-    public float[] getShapeX() {
-        return shapeX;
+    public void addFamily(Family family) {
+        this.families.add(family);
     }
 
-    public void setShapeX(float[] shapeX) {
-        this.shapeX = shapeX;
+    public void removeFromFamily(Family family) {
+        this.families.remove(family);
     }
 
-    public float[] getShapeY() {
-        return shapeY;
+    public boolean isMember(Family family) {
+        return families.contains(family);
     }
 
-    public void setShapeY(float[] shapeY) {
-        this.shapeY = shapeY;
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        Entity entity = (Entity) object;
+        if (entity.getID().equals(getID())) {
+            return true;
+        }
+
+        return false;
     }
 
     @Override
