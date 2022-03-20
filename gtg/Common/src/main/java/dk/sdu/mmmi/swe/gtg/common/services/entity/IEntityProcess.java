30235f8ea0ea415fdb19d017445b3936312a4adf
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-03-20 01:50:02 +0100

    Delete IEntityProcess

--- gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/services/entity/IEntityProcess.java
+++
@@ -1,8 +0,0 @@
-package dk.sdu.mmmi.swe.gtg.common.services.entity;
-
-import dk.sdu.mmmi.swe.gtg.common.data.GameData;
-import dk.sdu.mmmi.swe.gtg.common.data.World;
-
-public interface IEntityProcess {
-    void process(GameData gameData, World world);
-}
