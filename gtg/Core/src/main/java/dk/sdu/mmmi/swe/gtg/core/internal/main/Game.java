1d71567a6b1f891f201198a168e1b35fb457fbf3
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-03-20 01:47:44 +0100

    Updated Game

--- gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/main/Game.java
+++ gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/main/Game.java
@@ -6,14 +6,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import dk.sdu.mmmi.swe.gtg.common.data.Entity;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
-import dk.sdu.mmmi.swe.gtg.common.data.World;
-import dk.sdu.mmmi.swe.gtg.common.services.entity.IEntityProcessingService;
-import dk.sdu.mmmi.swe.gtg.common.services.entity.IPostEntityProcessingService;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IGamePluginService;
 import dk.sdu.mmmi.swe.gtg.core.internal.managers.GameInputProcessor;
+import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 
 import java.util.Collection;
 import java.util.List;
@@ -22,15 +18,12 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class Game implements ApplicationListener {
 
     private static OrthographicCamera cam;
-    private ShapeRenderer sr;
 
     private final GameData gameData = new GameData();
 
-    private World world = new World();
-
-    private List<IEntityProcessingService> entityProcessors = new CopyOnWriteArrayList<>();
-    private List<IPostEntityProcessingService> entityPostProcessors = new CopyOnWriteArrayList<>();
     private List<IGamePluginService> entityPlugins = new CopyOnWriteArrayList<>();
+
+    private IEngine engine;
 
     public Game() {
         init();
@@ -55,8 +48,6 @@ public class Game implements ApplicationListener {
         cam = new OrthographicCamera(gameData.getDisplayWidth(), gameData.getDisplayHeight());
         cam.translate(gameData.getDisplayWidth() / 2, gameData.getDisplayHeight() / 2);
         cam.update();
-
-        sr = new ShapeRenderer();
 
         Gdx.input.setInputProcessor(
                 new GameInputProcessor(gameData)
@@ -95,72 +86,32 @@ public class Game implements ApplicationListener {
     }
 
     private void update() {
-        getEntityProcessingServices().forEach(entityProcessor -> {
-            entityProcessor.process(gameData, world);
-        });
-
-        getPostEntityProcessingServices().forEach(entityPostProcessor -> {
-            entityPostProcessor.process(gameData, world);
-        });
+        engine.update(gameData);
     }
 
     private void draw() {
-        for (Entity entity : world.getEntities()) {
 
-            sr.setColor(1, 1, 1, 1);
-
-            sr.begin(ShapeRenderer.ShapeType.Filled);
-
-            float[] shapex = entity.getShapeX();
-            float[] shapey = entity.getShapeY();
-
-            for (int i = 0, j = shapex.length - 1;
-                 i < shapex.length;
-                 j = i++) {
-
-                sr.line(shapex[i], shapey[i], shapex[j], shapey[j]);
-            }
-
-            sr.end();
-        }
     }
 
     private Collection<? extends IGamePluginService> getPluginServices() {
         return entityPlugins;
     }
 
-    private Collection<? extends IEntityProcessingService> getEntityProcessingServices() {
-        return entityProcessors;
-    }
-
-    private Collection<? extends IPostEntityProcessingService> getPostEntityProcessingServices() {
-        return entityPostProcessors;
-    }
-
     public void addGamePluginService(IGamePluginService plugin) {
         this.entityPlugins.add(plugin);
-        plugin.start(gameData, world);
+        plugin.start(gameData);
     }
 
     public void removeGamePluginService(IGamePluginService plugin) {
         this.entityPlugins.remove(plugin);
-        plugin.stop(gameData, world);
+        plugin.stop(gameData);
     }
 
-    public void addEntityProcessingService(IEntityProcessingService service) {
-        this.entityProcessors.add(service);
+    public void setEngine(IEngine engine) {
+        this.engine = engine;
     }
 
-    public void removeEntityProcessingService(IEntityProcessingService service) {
-        this.entityProcessors.remove(service);
+    public void removeEngine(IEngine systemManager) {
+        this.engine = null;
     }
-
-    public void addPostEntityProcessingService(IPostEntityProcessingService service) {
-        this.entityPostProcessors.add(service);
-    }
-
-    public void removePostEntityProcessingService(IPostEntityProcessingService service) {
-        this.entityPostProcessors.remove(service);
-    }
-
 }
