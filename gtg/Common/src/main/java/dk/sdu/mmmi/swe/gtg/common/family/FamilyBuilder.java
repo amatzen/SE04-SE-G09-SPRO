445e1ecaf98464648218af7f8ee0ef14857aba5c
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-03-20 01:48:27 +0100

    Added FamilyBuilder

---
+++ gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/family/FamilyBuilder.java
@@ -0,0 +1,38 @@
+package dk.sdu.mmmi.swe.gtg.common.family;
+
+import dk.sdu.mmmi.swe.gtg.common.data.Entity;
+import dk.sdu.mmmi.swe.gtg.common.data.entityparts.EntityPart;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class FamilyBuilder implements IFamilyBuilder {
+
+    private Family family;
+
+    public FamilyBuilder() {
+        family = new Family();
+    }
+
+    @Override
+    public IFamilyBuilder forEntities(Class<? extends Entity> ... entities) {
+        Set<Class<? extends Entity>> entitySet = ConcurrentHashMap.newKeySet();
+        entitySet.addAll(Arrays.asList(entities));
+        family.setEntities(entitySet);
+        return this;
+    }
+
+    @Override
+    public IFamilyBuilder with(Class<? extends EntityPart> ... parts) {
+        Set<Class<? extends EntityPart>> partSet = ConcurrentHashMap.newKeySet();
+        partSet.addAll(Arrays.asList(parts));
+        family.setParts(partSet);
+        return this;
+    }
+
+    @Override
+    public Family get() {
+        return family;
+    }
+}
