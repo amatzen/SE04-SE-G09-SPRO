86fd0463a9f54192320f1f49cb0b44dd26fe25b7
Daniel Bahrami <daniel@bahrami.dk>
2022-03-22 20:07:48 +0100

    Implement methods

--- gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionControlSystem.java
+++ gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionControlSystem.java
@@ -1,29 +1,18 @@
 package dk.sdu.mmmi.swe.gtg.collision.internal;
 
-import dk.sdu.mmmi.swe.gtg.common.data.Entity;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
-import dk.sdu.mmmi.swe.gtg.common.data.World;
 import dk.sdu.mmmi.swe.gtg.common.services.entity.IEntityProcessingService;
+import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 
 public class CollisionControlSystem implements IEntityProcessingService {
 
     @Override
-    public void afterProcessing(GameData gameData, World world) {
+    public void addedToEngine(IEngine engine) {
 
     }
 
     @Override
-    public void beforeProcessing(GameData gameData, World world) {
-
-    }
-
-    @Override
-    public Class<Collision> getEntityType() {
-        return Collision.class;
-    }
-
-    @Override
-    public void processEntity(Entity entity, float deltaTime) {
+    public void process(GameData gameData) {
 
     }
 }
--- gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
+++ gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
@@ -1,18 +1,18 @@
 package dk.sdu.mmmi.swe.gtg.collision.internal;
 
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
-import dk.sdu.mmmi.swe.gtg.common.data.World;
+import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IGamePluginService;
 
 public class CollisionPlugin implements IGamePluginService {
 
     @Override
-    public void start(GameData gameData, World world) {
+    public void start(IEngine engine, GameData gameData) {
 
     }
 
     @Override
-    public void stop(GameData gameData, World world) {
+    public void stop(IEngine engine, GameData gameData) {
 
     }
 }
