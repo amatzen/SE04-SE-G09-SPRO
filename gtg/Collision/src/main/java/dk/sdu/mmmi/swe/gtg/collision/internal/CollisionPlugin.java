8f8f8b6648b1c2363de06e5120136573138ea2a4
Daniel Bahrami <daniel@bahrami.dk>
2022-03-22 22:52:41 +0100

    Add World Manager dependancy on module

--- gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
+++ gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
@@ -4,6 +4,7 @@ import dk.sdu.mmmi.swe.gtg.common.data.GameData;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IGamePluginService;
 import dk.sdu.mmmi.swe.gtg.shapefactorycommon.services.ShapeFactorySPI;
+import dk.sdu.mmmi.swe.gtg.worldmanager.services.IWorldManager;
 
 public class CollisionPlugin implements IGamePluginService {
 
