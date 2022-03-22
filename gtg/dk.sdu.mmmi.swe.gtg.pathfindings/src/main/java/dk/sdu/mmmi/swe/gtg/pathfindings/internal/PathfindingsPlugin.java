8cd3dddaa47a8a25da56548a2633bf04f3c98227
Alexander Matzen <alexander@amatzen.dk>
2022-03-22 23:12:35 +0100

    Some boilerplate that will be refactored

--- gtg/dk.sdu.mmmi.swe.gtg.pathfindings/src/main/java/dk/sdu/mmmi/swe/gtg/pathfindings/internal/PathfindingsPlugin.java
+++ gtg/dk.sdu.mmmi.swe.gtg.pathfindings/src/main/java/dk/sdu/mmmi/swe/gtg/pathfindings/internal/PathfindingsPlugin.java
@@ -1,4 +1,17 @@
 package dk.sdu.mmmi.swe.gtg.pathfindings.internal;
 
-public class PathfindingsPlugin {
+import dk.sdu.mmmi.swe.gtg.common.data.GameData;
+import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
+import dk.sdu.mmmi.swe.gtg.common.services.plugin.IGamePluginService;
+
+public class PathfindingsPlugin implements IGamePluginService {
+    public void start(IEngine engine, GameData gameData) {
+        System.out.println("Pathfindings Module Loaded");
+
+    }
+
+    public void stop(IEngine engine, GameData gameData) {
+        System.out.println("Pathfindings Module Unloaded");
+
+    }
 }
