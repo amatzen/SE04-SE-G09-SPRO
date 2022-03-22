99a104586e2394955684d94aa1e17452985502fe
Daniel Bahrami <daniel@bahrami.dk>
2022-03-22 20:24:33 +0100

    Add dependancy on module

--- gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
+++ gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
@@ -3,6 +3,7 @@ package dk.sdu.mmmi.swe.gtg.collision.internal;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IGamePluginService;
+import dk.sdu.mmmi.swe.gtg.shapefactorycommon.services.ShapeFactorySPI;
 
 public class CollisionPlugin implements IGamePluginService {
 
