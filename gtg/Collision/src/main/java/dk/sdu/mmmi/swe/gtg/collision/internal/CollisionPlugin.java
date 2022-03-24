411e7085aeb7e5134ca8cd8a73c5e2d41324db3c
Daniel Bahrami <daniel@bahrami.dk>
2022-03-24 12:14:49 +0100

    Added static body

--- gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
+++ gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
@@ -1,20 +1,49 @@
 package dk.sdu.mmmi.swe.gtg.collision.internal;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
+import dk.sdu.mmmi.swe.gtg.common.data.entityparts.BodyPart;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IGamePluginService;
 import dk.sdu.mmmi.swe.gtg.shapefactorycommon.services.ShapeFactorySPI;
-import dk.sdu.mmmi.swe.gtg.worldmanager.services.IWorldManager;
 
 public class CollisionPlugin implements IGamePluginService {
 
+    private ShapeFactorySPI shapeFactory;
+    private Collision collision;
+
     @Override
     public void start(IEngine engine, GameData gameData) {
+
+        Vector2 position = new Vector2(5, 0);
+        Vector2 size = new Vector2(1.0f, 1.0f);
+
+        collision = new Collision();
+
+        BodyPart body = new BodyPart(shapeFactory.createRectangle(
+                position, size, BodyDef.BodyType.StaticBody,
+                0.5f,
+                false));
+
+        collision.addPart(body);
+
+        engine.addEntity(collision);
+
+        System.out.println("CollisionPlugin started");
 
     }
 
     @Override
     public void stop(IEngine engine, GameData gameData) {
+        engine.removeEntity(collision);
+    }
 
+    public void setShapeFactory(ShapeFactorySPI shapeFactory) {
+        this.shapeFactory = shapeFactory;
+    }
+
+    public void removeShapeFactory(ShapeFactorySPI shapeFactory) {
+        this.shapeFactory = null;
     }
 }
