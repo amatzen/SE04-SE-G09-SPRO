1e55cb13ebffa90da82b2c2f13d4d9350f8b9fb3
Berkan Kütük <bekut20@student.sdu.dk>
2022-03-24 11:48:45 +0100

    Added collision detection for vehicles

---
+++ gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionDetector.java
@@ -0,0 +1,62 @@
+package dk.sdu.mmmi.swe.gtg.collision.internal;
+import dk.sdu.mmmi.swe.gtg.common.data.Entity;
+import dk.sdu.mmmi.swe.gtg.common.data.GameData;
+import dk.sdu.mmmi.swe.gtg.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.swe.gtg.common.family.Family;
+import dk.sdu.mmmi.swe.gtg.common.services.entity.IPostEntityProcessingService;
+import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
+import dk.sdu.mmmi.swe.gtg.vehicle.internal.Vehicle;
+
+import java.util.List;
+
+public class CollisionDetector implements IPostEntityProcessingService {
+
+    private IEngine engine;
+    private List<Vehicle> vehicleList;
+
+
+    @Override
+    public void addedToEngine(IEngine engine) {
+        this.engine = engine;
+
+        vehicleList = (List<Vehicle>) engine.getEntitiesFor(
+                Family.builder().forEntities(Vehicle.class).get()
+        );
+    }
+
+    @Override
+    public void process(GameData gameData) {
+
+        for (Vehicle vehicle : vehicleList ) {
+            for (Vehicle vehicle2 : vehicleList) {
+
+                // If the two entities are identical, skip the iteration
+                if (vehicle.getID().equals(vehicle2.getID())) {
+                    continue;
+                }
+
+                // CollisionDetection logic
+                if (checkCollision(vehicle, vehicle2)) {
+                    System.out.println("Collision detected!");
+                }
+            }
+        }
+    }
+
+    // A function that checks collisions
+    public Boolean checkCollision(Vehicle entity, Vehicle entity2) {
+        PositionPart entMov = entity.getPart(PositionPart.class);
+        PositionPart entMov2 = entity2.getPart(PositionPart.class);
+
+        float dx = entMov.getX() - entMov2.getX();
+        float dy = entMov.getY() - entMov2.getY();
+        float distance = (float) Math.sqrt(dx * dx + dy * dy);
+
+        // Calculates collision between two entities
+        if (distance < (entity.getRadius() + entity2.getRadius())) {
+            return true;
+        }
+        return false;
+    }
+
+}
