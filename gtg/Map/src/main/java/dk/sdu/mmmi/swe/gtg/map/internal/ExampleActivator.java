37591d2e049b76449f793db28fbd49e8cea673f3
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-03-24 18:56:20 +0100

    Added Map bundle

---
+++ gtg/Map/src/main/java/dk/sdu/mmmi/swe/gtg/map/internal/ExampleActivator.java
@@ -0,0 +1,45 @@
+package dk.sdu.mmmi.swe.gtg.map.internal;
+
+import java.util.Dictionary;
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import dk.sdu.mmmi.swe.gtg.map.ExampleService;
+
+/**
+ * Extension of the default OSGi bundle activator
+ */
+public final class ExampleActivator
+    implements BundleActivator
+{
+    /**
+     * Called whenever the OSGi framework starts our bundle
+     */
+    public void start( BundleContext bc )
+        throws Exception
+    {
+        System.out.println( "STARTING dk.sdu.mmmi.swe.gtg.map" );
+
+        Dictionary props = new Properties();
+        // add specific service properties here...
+
+        System.out.println( "REGISTER dk.sdu.mmmi.swe.gtg.map.ExampleService" );
+
+        // Register our example service implementation in the OSGi service registry
+        bc.registerService( ExampleService.class.getName(), new ExampleServiceImpl(), props );
+    }
+
+    /**
+     * Called whenever the OSGi framework stops our bundle
+     */
+    public void stop( BundleContext bc )
+        throws Exception
+    {
+        System.out.println( "STOPPING dk.sdu.mmmi.swe.gtg.map" );
+
+        // no need to unregister our service - the OSGi framework handles it for us
+    }
+}
+
