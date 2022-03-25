068b81a5af06ab52ce67a0b74b39012f7b6653fd
Alexander Matzen <alexander@amatzen.dk>
2022-03-25 15:21:26 +0100

    Outsourced game render logic to gamesreen

--- gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/main/Game.java
+++ gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/main/Game.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IGamePluginService;
+import dk.sdu.mmmi.swe.gtg.core.internal.managers.GameInputProcessor;
 import dk.sdu.mmmi.swe.gtg.core.internal.screens.GameScreen;
 import dk.sdu.mmmi.swe.gtg.core.internal.screens.SplashScreen;
 import dk.sdu.mmmi.swe.gtg.worldmanager.services.IWorldManager;
@@ -25,6 +26,7 @@ public class Game extends com.badlogic.gdx.Game implements ApplicationListener {
     private List<IGamePluginService> entityPlugins = new CopyOnWriteArrayList<>();
 
     private IEngine engine;
+    private IWorldManager worldManager;
 
     public Game() {
         init();
@@ -46,29 +48,16 @@ public class Game extends com.badlogic.gdx.Game implements ApplicationListener {
         gameData.setDisplayWidth(Gdx.graphics.getWidth());
         gameData.setDisplayHeight(Gdx.graphics.getHeight());
 
-        /*
         Gdx.input.setInputProcessor(
                 new GameInputProcessor(gameData)
-        );*/
+        );
         setScreen(new SplashScreen(this));
 
     }
 
     @Override
     public void render() {
-/*
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        gameData.setDelta(Gdx.graphics.getDeltaTime());
-
-        update();
-
-        draw();*/
-
         super.render();
-
-        gameData.getKeys().update();
     }
 
     @Override
@@ -88,16 +77,6 @@ public class Game extends com.badlogic.gdx.Game implements ApplicationListener {
     public void dispose() {
     }
 
-    private void update() {
-        engine.update(gameData);
-
-        //worldManager.update(gameData.getDelta());
-    }
-
-    private void draw() {
-        //worldManager.render(mB2dr, cam.combined);
-    }
-
     private Collection<? extends IGamePluginService> getPluginServices() {
         return entityPlugins;
     }
@@ -112,11 +91,27 @@ public class Game extends com.badlogic.gdx.Game implements ApplicationListener {
         plugin.stop(engine, gameData);
     }
 
+    public IEngine getEngine() {
+        return engine;
+    }
+
     public void setEngine(IEngine engine) {
         this.engine = engine;
     }
 
     public void removeEngine(IEngine systemManager) {
         this.engine = null;
+    }
+
+    public IWorldManager getWorldManager() {
+        return worldManager;
+    }
+
+    public void setWorldManager(IWorldManager worldManager) {
+        this.worldManager = worldManager;
+    }
+
+    public void removeWorldManager() {
+        this.worldManager = null;
     }
 }
