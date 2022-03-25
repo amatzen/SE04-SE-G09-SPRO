275a945825b4a1cd12411ede61f6951c65aa7126
Alexander Matzen <alexander@amatzen.dk>
2022-03-25 15:21:12 +0100

    Game screen finally works

--- gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/screens/GameScreen.java
+++ gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/screens/GameScreen.java
@@ -1,12 +1,14 @@
 package dk.sdu.mmmi.swe.gtg.core.internal.screens;
 
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
 import dk.sdu.mmmi.swe.gtg.core.internal.main.Game;
+import dk.sdu.mmmi.swe.gtg.worldmanager.internal.WorldManager;
 import dk.sdu.mmmi.swe.gtg.worldmanager.services.IWorldManager;
 
 public class GameScreen extends ScreenAdapter implements Screen {
@@ -23,16 +25,26 @@ public class GameScreen extends ScreenAdapter implements Screen {
         this.game = game;
         this.gameData = game.gameData;
 
-        mB2dr = new Box2DDebugRenderer();
-        cam = new OrthographicCamera(gameData.getDisplayWidth() / PPM, gameData.getDisplayHeight() / PPM);
+        this.mB2dr = new Box2DDebugRenderer();
+        this.cam = new OrthographicCamera(gameData.getDisplayWidth() / PPM, gameData.getDisplayHeight() / PPM);
+        this.worldManager = this.game.getWorldManager();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
 
-        worldManager.update(gameData.getDelta());
-        worldManager.render(mB2dr, cam.combined);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        gameData.setDelta(Gdx.graphics.getDeltaTime());
+
+        this.game.getEngine().update(this.gameData);
+
+        this.game.getWorldManager().update(this.gameData.getDelta());
+        this.game.getWorldManager().render(mB2dr, cam.combined);
+
+        this.gameData.getKeys().update();
     }
 
     @Override
