9f77eda427df2cf1d9fab8cb8897107bcac002f9
Alexander Matzen <alexander@amatzen.dk>
2022-03-25 12:00:42 +0100

    Changes

--- gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/screens/SplashScreen.java
+++ gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/screens/SplashScreen.java
@@ -34,6 +34,7 @@ public class SplashScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
+        sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         sprite.draw(batch);
         batch.end();
 
