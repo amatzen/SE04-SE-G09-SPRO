d80ca4af096dc59f6ab24546c8dda7fe4858ae7e
Daniel Bahrami <daniel@bahrami.dk>
2022-03-25 12:51:39 +0100

    Add ContactListener

---
+++ gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/ContactListener.java
@@ -0,0 +1,28 @@
+package dk.sdu.mmmi.swe.gtg.collision.internal;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+public class ContactListener implements com.badlogic.gdx.physics.box2d.ContactListener {
+
+    @Override
+    public void beginContact(Contact contact) {
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold manifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse contactImpulse) {
+
+    }
+}
