d11fff72e880a358fb31792a077a59cf09df0fc1
jasan <jasanfarah@gmail.com>
2022-03-26 12:38:31 +0100

    testing

--- gtg/Bullet/src/main/java/dk/sdu/mmmi/swe/gtg/Bullet/BulletCreator.java
+++ gtg/Bullet/src/main/java/dk/sdu/mmmi/swe/gtg/Bullet/BulletCreator.java
@@ -1,11 +1,11 @@
 package dk.sdu.mmmi.swe.gtg.Bullet;
 import dk.sdu.mmmi.swe.gtg.common.data.Entity;
-import dk.sdu.mmmi.swe.gtg.common.data.GameData;
 import dk.sdu.mmmi.swe.gtg.commonbullet.BulletSPI;
 
 public class BulletCreator implements BulletSPI {
     @Override
     public Entity createBullet() {
+        System.out.println("skrr");
         return null;
     }
 }
