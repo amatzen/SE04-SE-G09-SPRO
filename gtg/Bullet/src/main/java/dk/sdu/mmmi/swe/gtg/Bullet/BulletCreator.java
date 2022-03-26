38d717c5476bbd1e7268fd2d783f11c8c98f3451
jasan <jasanfarah@gmail.com>
2022-03-26 12:36:28 +0100

    Added dependency for commonbullet and implemented SPI

--- gtg/Bullet/src/main/java/dk/sdu/mmmi/swe/gtg/Bullet/BulletCreator.java
+++ gtg/Bullet/src/main/java/dk/sdu/mmmi/swe/gtg/Bullet/BulletCreator.java
@@ -1,4 +1,11 @@
 package dk.sdu.mmmi.swe.gtg.Bullet;
+import dk.sdu.mmmi.swe.gtg.common.data.Entity;
+import dk.sdu.mmmi.swe.gtg.common.data.GameData;
+import dk.sdu.mmmi.swe.gtg.commonbullet.BulletSPI;
 
-public class BulletCreator {
+public class BulletCreator implements BulletSPI {
+    @Override
+    public Entity createBullet(Entity vehicleEntity, GameData gameData) {
+        return null;
+    }
 }
