d0c520de2c6d2d8f9ff9ac356735c6e70b1bc8cb
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-03-29 22:35:43 +0200

    Added TransformPart

--- gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/TransformPart.java
+++ gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/TransformPart.java
@@ -1,10 +1,14 @@
 package dk.sdu.mmmi.swe.gtg.common.data.entityparts;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 public class TransformPart implements IEntityPart {
 
     private final Vector3 position = new Vector3();
+    private final Vector2 scale = new Vector2(1, 1);
+    private final Vector2 origin = new Vector2();
+
     private float rotation = 0.0f;
 
     public void setPosition(float x, float y){
@@ -19,8 +23,28 @@ public class TransformPart implements IEntityPart {
         return position;
     }
 
+    public void setOrigin(float x, float y){
+        this.origin.set(x, y);
+    }
+
+    public Vector2 getOrigin() {
+        return origin;
+    }
+
     public void setRotation(float rot){
         this.rotation = rot;
+    }
+
+    public float getRotation() {
+        return rotation;
+    }
+
+    public Vector2 getScale() {
+        return scale;
+    }
+
+    public void setScale(float x, float y){
+        this.scale.set(x, y);
     }
 
     @Override
