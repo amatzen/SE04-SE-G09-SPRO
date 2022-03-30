75763cdc98bcddfc4e85d6dc6fbb30d8f0afab58
Berkanktk <berkank@live.dk>
2022-03-30 13:43:09 +0200

    Removed non-used imports

--- gtg/Map/src/main/java/dk/sdu/mmmi/swe/gtg/map/internal/MapControlSystem.java
+++ gtg/Map/src/main/java/dk/sdu/mmmi/swe/gtg/map/internal/MapControlSystem.java
@@ -1,9 +1,7 @@
 package dk.sdu.mmmi.swe.gtg.map.internal;
 
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import dk.sdu.mmmi.swe.gtg.common.data.Entity;
