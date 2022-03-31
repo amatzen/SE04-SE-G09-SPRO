e38146cb173bf01280d496333c6ea92109b81605
Berkan Kütük <bekut20@student.sdu.dk>
2022-03-31 11:48:42 +0200

    Removed old resource files

--- gtg/Map/src/main/resources/Water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Water" tilewidth="64" tileheight="64" tilecount="16" columns="16">
- <image source="Water.png" width="1024" height="64"/>
-</tileset>
--- gtg/Map/src/main/resources/Windows.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Windows" tilewidth="64" tileheight="64" tilecount="128" columns="32">
- <image source="Windows.png" width="2048" height="256"/>
-</tileset>
