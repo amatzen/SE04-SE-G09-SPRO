e71f85df3d9e3f3f2dbfbb7d14c595c291a89e9b
Berkanktk <berkank@live.dk>
2022-03-31 19:13:04 +0200

    Added lofi background music

--- gtg/Music/src/main/java/dk/sdu/mmmi/swe/gtg/music/internal/MusicControlSystem.java
+++ gtg/Music/src/main/java/dk/sdu/mmmi/swe/gtg/music/internal/MusicControlSystem.java
@@ -1,5 +1,7 @@
 package dk.sdu.mmmi.swe.gtg.music.internal;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
 import dk.sdu.mmmi.swe.gtg.common.services.entity.IEntityProcessingService;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
@@ -8,13 +10,18 @@ import org.osgi.service.component.annotations.Component;
 @Component
 public class MusicControlSystem implements IEntityProcessingService {
 
+    private Music music;
+
     @Override
     public void addedToEngine(IEngine engine) {
-
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/lofi.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.5f);
+        music.play();
     }
 
     @Override
     public void process(GameData gameData) {
-        
+
     }
 }
