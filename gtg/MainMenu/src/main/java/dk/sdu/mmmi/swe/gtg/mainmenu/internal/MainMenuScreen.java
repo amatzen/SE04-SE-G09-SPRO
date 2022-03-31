9b40ebaf494b9aa83178af60b956cf15ab0917bc
Alexander Matzen <alexander@amatzen.dk>
2022-03-31 13:04:27 +0200

    Module

---
+++ gtg/MainMenu/src/main/java/dk/sdu/mmmi/swe/gtg/mainmenu/internal/MainMenuScreen.java
@@ -0,0 +1,36 @@
+package dk.sdu.mmmi.swe.gtg.mainmenu.internal;
+
+import com.badlogic.gdx.Screen;
+import dk.sdu.mmmi.swe.gtg.common.screens.IMainMenuScreen;
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class MainMenuScreen implements IMainMenuScreen, Screen {
+    public void show() {
+
+    }
+
+    public void render(float v) {
+
+    }
+
+    public void resize(int i, int i1) {
+
+    }
+
+    public void pause() {
+
+    }
+
+    public void resume() {
+
+    }
+
+    public void hide() {
+
+    }
+
+    public void dispose() {
+
+    }
+}
