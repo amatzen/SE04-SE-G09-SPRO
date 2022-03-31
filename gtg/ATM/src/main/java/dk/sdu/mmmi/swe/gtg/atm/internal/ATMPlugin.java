872c349cb3f61007e4ea3b615eb9d2f17679cabb
Daniel Bahrami <daniel@bahrami.dk>
2022-03-31 13:46:04 +0200

    Add ATM module

---
+++ gtg/ATM/src/main/java/dk/sdu/mmmi/swe/gtg/atm/internal/ATMPlugin.java
@@ -0,0 +1,18 @@
+package dk.sdu.mmmi.swe.gtg.atm.internal;
+
+import dk.sdu.mmmi.swe.gtg.common.data.GameData;
+import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
+import dk.sdu.mmmi.swe.gtg.common.services.plugin.IGamePluginService;
+
+public class ATMPlugin implements IGamePluginService {
+
+    @Override
+    public void start(IEngine engine, GameData gameData) {
+
+    }
+
+    @Override
+    public void stop(IEngine engine, GameData gameData) {
+
+    }
+}
