105ee5e0c15afc22d6855ac296c2773719119b15
Berkanktk <berkank@live.dk>
2022-03-31 18:23:07 +0200

    Created music module

---
+++ gtg/Music/src/main/java/Music/internal/MusicControlSystem.java
@@ -0,0 +1,16 @@
+package Music.internal;
+
+import dk.sdu.mmmi.swe.gtg.common.data.GameData;
+import dk.sdu.mmmi.swe.gtg.common.services.entity.IEntityProcessingService;
+import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
+
+public class MusicControlSystem implements IEntityProcessingService {
+
+    public void addedToEngine(IEngine engine) {
+
+    }
+
+    public void process(GameData gameData) {
+
+    }
+}
