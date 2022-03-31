f13ab11fb4443d355e41237f2051c2eb19a603af
Alexander Matzen <alexander@amatzen.dk>
2022-03-31 13:20:46 +0200

    fix err

--- gtg/MainMenu/src/main/java/dk/sdu/mmmi/swe/gtg/mainmenu/internal/MainMenuScreen.java
+++ gtg/MainMenu/src/main/java/dk/sdu/mmmi/swe/gtg/mainmenu/internal/MainMenuScreen.java
@@ -1,11 +1,8 @@
 package dk.sdu.mmmi.swe.gtg.mainmenu.internal;
 
 import com.badlogic.gdx.Screen;
-import dk.sdu.mmmi.swe.gtg.common.screens.IMainMenuScreen;
-import org.osgi.service.component.annotations.Component;
 
-@Component
-public class MainMenuScreen implements IMainMenuScreen, Screen {
+public class MainMenuScreen implements Screen {
     public void show() {
 
     }
