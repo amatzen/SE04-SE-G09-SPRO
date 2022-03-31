da37b10733960609ae88557c8a309892303fa278
Alexander Matzen <alexander@amatzen.dk>
2022-03-31 13:53:43 +0200

    Fixed issue with game input after embedding main menu

--- gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/main/Game.java
+++ gtg/Core/src/main/java/dk/sdu/mmmi/swe/gtg/core/internal/main/Game.java
@@ -56,9 +56,6 @@ public class Game extends com.badlogic.gdx.Game implements ApplicationListener {
         gameData.setDisplayWidth(Gdx.graphics.getWidth());
         gameData.setDisplayHeight(Gdx.graphics.getHeight());
 
-        Gdx.input.setInputProcessor(
-                new GameInputProcessor(gameData)
-        );
         setScreen(new SplashScreen(this));
 
     }
