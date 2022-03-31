e7097a5af8a3f831577590a0125b9653cbcec8b4
Berkanktk <berkank@live.dk>
2022-03-31 19:04:26 +0200

    Refactoring and setup

--- gtg/Music/src/main/java/dk/sdu/mmmi/swe/gtg/music/internal/MusicControlSystem.java
+++ gtg/Music/src/main/java/dk/sdu/mmmi/swe/gtg/music/internal/MusicControlSystem.java
@@ -1,16 +1,20 @@
-package Music.internal;
+package dk.sdu.mmmi.swe.gtg.music.internal;
 
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
 import dk.sdu.mmmi.swe.gtg.common.services.entity.IEntityProcessingService;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
+import org.osgi.service.component.annotations.Component;
 
+@Component
 public class MusicControlSystem implements IEntityProcessingService {
 
+    @Override
     public void addedToEngine(IEngine engine) {
 
     }
 
+    @Override
     public void process(GameData gameData) {
-
+        
     }
 }
