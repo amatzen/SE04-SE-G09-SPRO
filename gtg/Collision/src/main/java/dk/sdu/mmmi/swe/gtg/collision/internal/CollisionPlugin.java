839131cc827e4c9c09adb450f5b1efaf9290051c
Daniel Bahrami <daniel@bahrami.dk>
2022-03-31 19:12:29 +0200

    Reformat code

--- gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
+++ gtg/Collision/src/main/java/dk/sdu/mmmi/swe/gtg/collision/internal/CollisionPlugin.java
@@ -1,16 +1,8 @@
 package dk.sdu.mmmi.swe.gtg.collision.internal;
 
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import dk.sdu.mmmi.swe.gtg.collision.Collision;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
-import dk.sdu.mmmi.swe.gtg.common.data.entityparts.BodyPart;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IGamePluginService;
-import dk.sdu.mmmi.swe.gtg.shapefactorycommon.services.ShapeFactorySPI;
-import dk.sdu.mmmi.swe.gtg.worldmanager.services.IWorldManager;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 
 public class CollisionPlugin implements IGamePluginService {
