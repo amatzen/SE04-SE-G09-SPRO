7cb915a5b0dd3672f7b5a274639c97359c43dcfc
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-04-09 23:27:18 +0200

    Added SteeringPart

--- gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/SteeringPart.java
+++ gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/SteeringPart.java
@@ -1,8 +1,30 @@
 package dk.sdu.mmmi.swe.gtg.common.data.entityparts;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class SteeringPart implements IEntityPart {
+
+    private Vector2 desiredVelocity;
+    private Vector2 steering;
+
     @Override
     public void destroy() {
 
+    }
+
+    public void setDesiredVelocity(Vector2 desiredVelocity) {
+        this.desiredVelocity = desiredVelocity;
+    }
+
+    public void setSteering(Vector2 steering) {
+        this.steering = steering;
+    }
+
+    public Vector2 getDesiredVelocity() {
+        return desiredVelocity;
+    }
+
+    public Vector2 getSteering() {
+        return steering;
     }
 }
