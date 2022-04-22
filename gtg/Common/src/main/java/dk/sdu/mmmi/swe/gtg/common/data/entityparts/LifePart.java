87b469b98d3f64f40cc1ffcfd72508e2d63383ed
Jasan <jasan@jasan.io>
2022-04-22 15:48:57 +0200

    lifepart getter and setter

--- gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/LifePart.java
+++ gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/LifePart.java
@@ -1,9 +1,19 @@
 package dk.sdu.mmmi.swe.gtg.common.data.entityparts;
 
 public class LifePart implements IEntityPart {
-    
+
+    private int life;
     @Override
     public void destroy() {
 
+    }
+
+    public int getLife(){
+        return life;
+    }
+
+    public void setLife(int life, int damage){
+        this.life= life-damage;
+        
     }
 }
