7dd40e8e1b03a063adbcd59c8c7bfa544278f710
Jasan <jasan@jasan.io>
2022-04-29 15:11:19 +0200

    siuuu

--- gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/LifePart.java
+++ gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/LifePart.java
@@ -3,7 +3,6 @@ package dk.sdu.mmmi.swe.gtg.common.data.entityparts;
 public class LifePart implements IEntityPart {
 
     private int life = 100;
-    private int life;
 
     @Override
     public void destroy() {
@@ -17,11 +16,7 @@ public class LifePart implements IEntityPart {
     public void setLife(int life){
         this.life= life;
     }
-    public void setDamage(int damage){
-        this.life = life-damage;
-      
-    public void setLife(int life, int damage) {
+    public void setDamage(int damage) {
         this.life = life - damage;
-
     }
 }
