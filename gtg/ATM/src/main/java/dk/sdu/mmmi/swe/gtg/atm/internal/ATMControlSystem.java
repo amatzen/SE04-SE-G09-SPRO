28e74226d6ee824d18a75cd75b141fdbaec13ad2
Alexander Matzen <alexander@amatzen.dk>
2022-04-29 14:36:58 +0200

    make component

--- gtg/ATM/src/main/java/dk/sdu/mmmi/swe/gtg/atm/internal/ATMControlSystem.java
+++ gtg/ATM/src/main/java/dk/sdu/mmmi/swe/gtg/atm/internal/ATMControlSystem.java
@@ -3,7 +3,9 @@ package dk.sdu.mmmi.swe.gtg.atm.internal;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
 import dk.sdu.mmmi.swe.gtg.common.services.entity.IEntityProcessingService;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
+import org.osgi.service.component.annotations.Component;
 
+@Component
 public class ATMControlSystem implements IEntityProcessingService {
 
     @Override
