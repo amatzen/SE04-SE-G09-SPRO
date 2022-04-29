f565e58fb821302de0d4a558d8a6c1680e618a04
Alexander Matzen <alexander@amatzen.dk>
2022-04-29 15:56:25 +0200

    Added ATMBalancePart

---
+++ gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/ATMBalancePart.java
@@ -0,0 +1,10 @@
+package dk.sdu.mmmi.swe.gtg.common.data.entityparts;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class ATMBalancePart implements IEntityPart {
+    @Override
+    public void destroy() {
+        throw new NotImplementedException();
+    }
+}
