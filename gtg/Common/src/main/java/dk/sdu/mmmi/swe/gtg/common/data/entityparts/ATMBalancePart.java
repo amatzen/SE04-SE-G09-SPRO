9f4d55e96a168fc4ad813ea5bd1660c0d4d5d067
Alexander Matzen <alexander@amatzen.dk>
2022-05-06 13:38:27 +0200

    fix err

--- gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/ATMBalancePart.java
+++ gtg/Common/src/main/java/dk/sdu/mmmi/swe/gtg/common/data/entityparts/ATMBalancePart.java
@@ -1,10 +1,8 @@
 package dk.sdu.mmmi.swe.gtg.common.data.entityparts;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class ATMBalancePart implements IEntityPart {
     @Override
     public void destroy() {
-        throw new NotImplementedException();
+        System.out.println("destroy");
     }
 }
