50da003dceac1b74823a956c9634dd5fdfa9bd57
Berkan Kütük <bekut20@student.sdu.dk>
2022-05-06 15:52:39 +0200

    Added a gameover SPI

---
+++ gtg/CommonGameOver/src/main/java/dk/sdu/mmmi/swe/gtg/commongameover/GameOverSPI.java
@@ -0,0 +1,8 @@
+package dk.sdu.mmmi.swe.gtg.commongameover;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public interface GameOverSPI {
+    public Stage getStage();
+    public Stage setStage();
+}
