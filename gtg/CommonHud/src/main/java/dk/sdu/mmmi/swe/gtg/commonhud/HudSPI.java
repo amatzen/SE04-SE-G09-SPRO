193dadac2c6669eca2c6dd2fe21fbe6914aadf97
Jasan <jasan@jasan.io>
2022-05-06 14:38:01 +0200

    SPI Contract updated

--- gtg/CommonHud/src/main/java/dk/sdu/mmmi/swe/gtg/commonhud/HudSPI.java
+++ gtg/CommonHud/src/main/java/dk/sdu/mmmi/swe/gtg/commonhud/HudSPI.java
@@ -3,4 +3,8 @@ package dk.sdu.mmmi.swe.gtg.commonhud;
 public interface HudSPI {
     public void setHealth(int value);
     public int getHealth();
+    public void setWanted();
+    public int getWanted();
+    public void setMoney();
+    public int getMoney();
 }
