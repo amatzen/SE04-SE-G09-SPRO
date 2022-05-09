c6ca8355e26191fdcf3a08c38210bd1f10963a2b
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-05-10 01:45:29 +0200

    Moved AI systems to separate components

---
+++ gtg/PathFindingCommon/src/main/java/dk/sdu/mmmi/swe/gtg/pathfindingcommon/data/Node.java
@@ -0,0 +1,68 @@
+package dk.sdu.mmmi.swe.gtg.pathfindingcommon.data;
+
+import com.badlogic.gdx.math.Vector2;
+public class Node {
+
+    private Node parent;
+    private Vector2 state;
+    private float cost;
+
+    public Node() {
+        this.parent = null;
+        this.state = null;
+        this.cost = 0;
+    }
+
+    public Node(Node parent, Vector2 state, float cost) {
+        this.parent = parent;
+        this.state = state;
+        this.cost = cost;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public Vector2 getState() {
+        return state;
+    }
+
+    public void setState(Vector2 state) {
+        this.state = state;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public int hashCode() {
+        return ((int) state.y) * 1000 + ((int) state.x);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (!(obj instanceof Node)) return false;
+
+        return hashCode() == obj.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return state.toString();
+    }
+
+    public String getKey() {
+        return (int) this.getState().x + "," + (int) this.getState().y;
+    }
+}
