9fb37bddb7f113f388b161f61684d5bd6e2722cc
Daniel Bahrami <daniel@bahrami.dk>
2022-05-10 21:29:51 +0200

    Change ATM texture after robbing

--- gtg/Crime/src/main/java/dk/sdu/mmmi/swe/gtg/crime/CrimeActionHandler.java
+++ gtg/Crime/src/main/java/dk/sdu/mmmi/swe/gtg/crime/CrimeActionHandler.java
@@ -1,7 +1,12 @@
 package dk.sdu.mmmi.swe.gtg.crime;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import dk.sdu.mmmi.swe.gtg.common.data.Entity;
 import dk.sdu.mmmi.swe.gtg.common.data.entityparts.ATMBalancePart;
+import dk.sdu.mmmi.swe.gtg.common.data.entityparts.TexturePart;
 import dk.sdu.mmmi.swe.gtg.commoncrime.ICrimeAction;
 import dk.sdu.mmmi.swe.gtg.commonhud.HudSPI;
 import org.osgi.service.component.annotations.Component;
@@ -18,7 +23,22 @@ public class CrimeActionHandler implements ICrimeAction {
         if (atmBalance != null) {
             hud.addMoney(atmBalance.getBalance());
             atmBalance.setRobbed(true);
+            entity.addPart(getBodyTexture());
             atmBalance.destroy();
         }
     }
+
+    private TexturePart getTexture(String path) {
+        final TexturePart texturePart = new TexturePart();
+        FileHandle file = Gdx.files.internal(path);
+        Texture texture = new Texture(file);
+        TextureRegion textureRegion = new TextureRegion(texture);
+        texturePart.setRegion(textureRegion);
+        return texturePart;
+    }
+
+    private TexturePart getBodyTexture() {
+        return getTexture("assets/atmRobbed.png");
+    }
+
 }
