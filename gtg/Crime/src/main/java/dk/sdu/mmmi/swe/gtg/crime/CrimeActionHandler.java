d5570a860265c985cf37bf0586e8aa105207a304
Berkan Kütük <bekut20@student.sdu.dk>
2022-05-12 11:19:41 +0200

    Cash sound when money is stolen

--- gtg/Crime/src/main/java/dk/sdu/mmmi/swe/gtg/crime/CrimeActionHandler.java
+++ gtg/Crime/src/main/java/dk/sdu/mmmi/swe/gtg/crime/CrimeActionHandler.java
@@ -1,6 +1,7 @@
 package dk.sdu.mmmi.swe.gtg.crime;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -18,10 +19,17 @@ public class CrimeActionHandler implements ICrimeAction {
     @Reference
     private HudSPI hud;
 
+    public static Music cashSound;
+
     public void commit(Entity entity) {
         ATMBalancePart atmBalance = entity.getPart(ATMBalancePart.class);
         if (atmBalance != null) {
             hud.addMoney(atmBalance.getBalance());
+
+            cashSound = Gdx.audio.newMusic(Gdx.files.internal("sounds/Cash.mp3"));
+            cashSound.setVolume(0.3f);
+            cashSound.play();
+
             atmBalance.setRobbed(true);
             entity.addPart(getBodyTexture());
             atmBalance.destroy();
