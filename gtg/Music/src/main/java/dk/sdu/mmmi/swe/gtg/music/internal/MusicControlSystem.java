e5864c67b5381ebda0fde947696d500c9d889042
Berkan Kütük <bekut20@student.sdu.dk>
2022-05-12 10:13:56 +0200

    Police sirens only play when wanted

--- gtg/Music/src/main/java/dk/sdu/mmmi/swe/gtg/music/internal/MusicControlSystem.java
+++ gtg/Music/src/main/java/dk/sdu/mmmi/swe/gtg/music/internal/MusicControlSystem.java
@@ -1,18 +1,45 @@
 package dk.sdu.mmmi.swe.gtg.music.internal;
 
+import dk.sdu.mmmi.swe.gtg.common.data.Entity;
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
 import dk.sdu.mmmi.swe.gtg.common.data.GameKeys;
+import dk.sdu.mmmi.swe.gtg.common.data.entityparts.PlayerPart;
+import dk.sdu.mmmi.swe.gtg.common.data.entityparts.WantedPart;
+import dk.sdu.mmmi.swe.gtg.common.family.EntityListener;
+import dk.sdu.mmmi.swe.gtg.common.family.Family;
+import dk.sdu.mmmi.swe.gtg.common.family.IEntityListener;
 import dk.sdu.mmmi.swe.gtg.common.services.entity.IProcessingSystem;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
+import dk.sdu.mmmi.swe.gtg.commonhud.HudSPI;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 @Component
 public class MusicControlSystem implements IProcessingSystem {
 
+    private Entity player;
+
+    private IEntityListener playerListener;
+
+    private float crimeLevel;
 
     @Override
     public void addedToEngine(IEngine engine) {
+        crimeLevel = 0;
 
+        playerListener = new EntityListener() {
+            @Override
+            public void onEntityAdded(Entity entity) {
+                player = entity;
+            }
+
+            @Override
+            public void onEntityRemoved(Entity entity) {
+                player = null;
+            }
+        };
+
+        engine.addEntityListener(Family.builder().with(PlayerPart.class).get(), playerListener, true);
     }
 
     @Override
@@ -33,12 +60,24 @@ public class MusicControlSystem implements IProcessingSystem {
                 MusicPlugin.MenuMusic.stop();
                 MusicPlugin.GameSound.play();
             }
+            /*
             if (MusicPlugin.PoliceSound.isPlaying()) {
                 MusicPlugin.PoliceSound.pause();
             } else {
                 MusicPlugin.MenuMusic.stop();
                 MusicPlugin.PoliceSound.play();
             }
+
+             */
+        }
+
+        WantedPart wantedPart = player.getPart(WantedPart.class);
+        int totalWanted = wantedPart.getWantedLevel();
+
+        if (totalWanted > 0) {
+            MusicPlugin.PoliceSound.play();
+        } else if (totalWanted == 0){
+            MusicPlugin.PoliceSound.stop();
         }
     }
 }
