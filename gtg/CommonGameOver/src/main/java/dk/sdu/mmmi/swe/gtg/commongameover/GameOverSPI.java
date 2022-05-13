5aaf3ef58f39a8919cb5b025d1c98619d854e505
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-05-13 09:55:30 +0200

    Deleted GameOverSPI

--- gtg/CommonGameOver/src/main/java/dk/sdu/mmmi/swe/gtg/commongameover/GameOverSPI.java
+++
@@ -1,8 +0,0 @@
-package dk.sdu.mmmi.swe.gtg.commongameover;
-
-import com.badlogic.gdx.scenes.scene2d.Stage;
-
-public interface GameOverSPI {
-    public Stage getStage();
-    public Stage setStage();
-}
