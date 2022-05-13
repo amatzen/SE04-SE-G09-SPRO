484b39e2e53dded3489113c870d43e62e6199001
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-05-13 14:40:22 +0200

    Use dependency injection for IEngine in HudPlugin

--- gtg/Hud/src/main/java/dk/sdu/mmmi/swe/gtg/hud/internal/HudPlugin.java
+++ gtg/Hud/src/main/java/dk/sdu/mmmi/swe/gtg/hud/internal/HudPlugin.java
@@ -1,18 +1,17 @@
 package dk.sdu.mmmi.swe.gtg.hud.internal;
 
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
-import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IPlugin;
 
 public class HudPlugin implements IPlugin {
 
     @Override
-    public void install(IEngine engine, GameData gameData) {
+    public void install(GameData gameData) {
 
     }
 
     @Override
-    public void uninstall(IEngine engine, GameData gameData) {
+    public void uninstall(GameData gameData) {
 
     }
 }
