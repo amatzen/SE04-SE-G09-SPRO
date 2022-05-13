3305d3368bb11977c61ece11e13624cb6cf3bf43
Berkan Kütük <bekut20@student.sdu.dk>
2022-05-13 09:59:41 +0200

    Optimized imports for enemyplugin

--- gtg/EnemyAI/src/main/java/dk/sdu/mmmi/swe/gtg/enemyai/internal/EnemyPlugin.java
+++ gtg/EnemyAI/src/main/java/dk/sdu/mmmi/swe/gtg/enemyai/internal/EnemyPlugin.java
@@ -1,10 +1,8 @@
 package dk.sdu.mmmi.swe.gtg.enemyai.internal;
 
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
-import dk.sdu.mmmi.swe.gtg.common.family.Family;
 import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IPlugin;
-import dk.sdu.mmmi.swe.gtg.enemyai.Enemy;
 import org.osgi.service.component.annotations.Component;
 
 @Component
