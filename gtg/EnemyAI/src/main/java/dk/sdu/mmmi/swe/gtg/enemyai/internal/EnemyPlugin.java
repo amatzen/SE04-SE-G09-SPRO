7b769683429538482515226af54dffcd26e1c361
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2022-05-13 14:40:52 +0200

    Use dependency injection for IEngine in EnemyPlugin

--- gtg/EnemyAI/src/main/java/dk/sdu/mmmi/swe/gtg/enemyai/internal/EnemyPlugin.java
+++ gtg/EnemyAI/src/main/java/dk/sdu/mmmi/swe/gtg/enemyai/internal/EnemyPlugin.java
@@ -1,18 +1,17 @@
 package dk.sdu.mmmi.swe.gtg.enemyai.internal;
 
 import dk.sdu.mmmi.swe.gtg.common.data.GameData;
-import dk.sdu.mmmi.swe.gtg.common.services.managers.IEngine;
 import dk.sdu.mmmi.swe.gtg.common.services.plugin.IPlugin;
 import org.osgi.service.component.annotations.Component;
 
 @Component
 public class EnemyPlugin implements IPlugin {
     @Override
-    public void install(IEngine engine, GameData gameData) {
+    public void install(GameData gameData) {
     }
 
     @Override
-    public void uninstall(IEngine engine, GameData gameData) {
+    public void uninstall(GameData gameData) {
 
     }
 }
