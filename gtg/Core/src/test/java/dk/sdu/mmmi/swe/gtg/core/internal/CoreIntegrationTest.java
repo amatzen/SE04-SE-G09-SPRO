7050e1276835228418c5340b41e4060c1ce365d0
Alexander Matzen <alexander@amatzen.dk>
2022-05-13 14:38:38 +0200

    made it work

--- gtg/Core/src/test/java/dk/sdu/mmmi/swe/gtg/core/internal/CoreIntegrationTest.java
+++ gtg/Core/src/test/java/dk/sdu/mmmi/swe/gtg/core/internal/CoreIntegrationTest.java
@@ -4,11 +4,15 @@ import dk.sdu.mmmi.swe.gtg.common.data.Entity;
 import dk.sdu.mmmi.swe.gtg.common.data.entityparts.TransformPart;
 import dk.sdu.mmmi.swe.gtg.common.family.Family;
 import dk.sdu.mmmi.swe.gtg.common.family.IFamily;
+import dk.sdu.mmmi.swe.gtg.common.services.managers.ISystemManager;
 import dk.sdu.mmmi.swe.gtg.core.internal.managers.Engine;
 import dk.sdu.mmmi.swe.gtg.core.internal.managers.FamilyManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -18,15 +22,22 @@ public class CoreIntegrationTest {
     @Mock
     private FamilyManager familyManager;
 
-    @InjectMocks
+    @Mock
     private Engine engine;
+
+    @Mock
+    private Family family;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void engineFamilyManagerIntegrationTest() {
 
         Entity e = new Entity();
         e.addPart(new TransformPart());
-
 
         engine.addEntity(e);
 
