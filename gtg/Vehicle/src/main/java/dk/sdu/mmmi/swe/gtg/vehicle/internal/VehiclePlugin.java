c24e0425e13f4de55068cb73bf6d19d32ddf79ad
Daniel Bahrami <daniel@bahrami.dk>
2022-05-29 19:48:10 +0200

    Cleanup

--- gtg/Vehicle/src/main/java/dk/sdu/mmmi/swe/gtg/vehicle/internal/VehiclePlugin.java
+++ gtg/Vehicle/src/main/java/dk/sdu/mmmi/swe/gtg/vehicle/internal/VehiclePlugin.java
@@ -23,8 +23,6 @@ import dk.sdu.mmmi.swe.gtg.worldmanager.services.IWorldManager;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import java.util.Objects;
-
 @Component
 public class VehiclePlugin implements IPlugin {
 
